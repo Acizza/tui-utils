cf471751eafbdd4a817049d70afddd569fb39b60
Acizza <jgit@tuta.io>
2021-03-04 16:07:17 -0800

    Add latest stable Rust version to shell.nix buildInputs

--- shell.nix
+++ shell.nix
@@ -1,5 +1,17 @@
-with import <nixpkgs> {};
+{}:
 
-pkgs.mkShell {
-    buildInputs = [ stdenv.cc pkgconfig ];
+let
+  rustOverlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+
+  pkgs = import <nixpkgs> {
+    overlays = [ rustOverlay ];
+  };
+
+  rust = pkgs.rust-bin.stable.latest.rust.override {
+    extensions = [ "rust-src" ];
+  };
+in pkgs.mkShell {
+  buildInputs = with pkgs; [
+    rust
+  ];
 }
