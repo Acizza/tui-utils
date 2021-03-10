44e42b392004d8d5ed81631ae855b2252168d8b7
Acizza <jgit@tuta.io>
2021-03-10 14:45:44 -0800

    Remove simple prefix from widget module names

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,11 +1,11 @@
+pub mod list;
 pub mod log;
-pub mod simple_list;
-pub mod simple_text;
+pub mod text;
 pub mod text_fragments;
 
+pub use list::SimpleList;
 pub use log::Log;
-pub use simple_list::SimpleList;
-pub use simple_text::SimpleText;
+pub use text::SimpleText;
 pub use text_fragments::TextFragments;
 use tui::{style::Style, text::Span};
 
